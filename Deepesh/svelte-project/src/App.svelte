cb9502df069d3fa086d13f0a74554e44e9d51763
Deepesh Kumar <deepesh.kumar.ug21@nsut.ac.in>
2023-10-01 10:58:09 +0530

    added a basic counter

---
+++ Deepesh/svelte-project/src/App.svelte
@@ -0,0 +1,29 @@
+<script>
+  let count = 0;
+
+  function increment() {
+    count += 1;
+  }
+
+  function decrement() {
+    count -= 1;
+  }
+</script>
+
+<main>
+  <h1>Hello Svelte!</h1>
+  <p>Count: {count}</p>
+  <button on:click={increment}>Increment</button>
+  <button on:click={decrement}>Decrement</button>
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 20px;
+  }
+  button {
+    font-size: 1.2rem;
+    margin: 5px;
+  }
+</style>
---
+++ Deepesh/svelte-project/src/Components/Header.svelte
@@ -0,0 +1,15 @@
+<script>
+  let title = 'My Svelte App';
+</script>
+
+<header>
+  <h1>{title}</h1>
+</header>
+
+<style>
+  header {
+    background-color: #333;
+    color: white;
+    padding: 10px;
+  }
+</style>
